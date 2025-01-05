1888a1a423fefd19f0e7669ac12a65c48785762f
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-01-05 19:08:03 +0900

    [level 0] Title: 수 조작하기 2, Time: 1167.27 ms, Memory: 383 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,21 +1,22 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
+        
         for (int i = 1; i < numLog.length; i++) {
             if (numLog[i] - numLog[i-1] == 1) {
-                 sb.append("w");
+                 answer += "w";
             }
             else if (numLog[i] - numLog[i-1] == -1) {
-                 sb.append("s");
+                 answer += "s";
             }
             else if (numLog[i] - numLog[i-1] == 10) {
-                 sb.append("d");
+                 answer += "d";
             }
             else  {
-                 sb.append("a");
+                 answer += "a";
             }
        }
-        return sb.toString();
+        return answer;
     }
 }
+
