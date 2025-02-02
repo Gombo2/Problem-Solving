c37e9dbc49ca0b5c5cdae3966bd58e2fa0d6c18f
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-02 19:26:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 132.94 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,37 +1,42 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
         for(int i = 0; i < s.length(); i++) {
             if(i == 0) {
-              if(isTrue(s)) {
-                  answer++;
-              }
+                if(isTrue(s)) {
+                    answer++;
+                }
             } else {
                 s = rotateRight(s);
                 if(isTrue(s)) {
-                  answer++;
-              }
+                    answer++;
+                }
             }
         }
         return answer;
     }
-    
-    private String rotateRight(String s) {
+     private String rotateRight(String s) {
         return s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
     }
     
     private boolean isTrue(String s) {
-        int prevLength;
-        String small = "\\(\\)"; 
-        String medium = "\\[\\]";
-        String big = "\\{\\}";
-        do {
-            prevLength = s.length();
-            s = s.replaceAll(small, "")
-                .replaceAll(medium, "")
-                .replaceAll(big, "");
-        } while (s.length() < prevLength);
-
-        return s.length() == 0 ? true : false;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(st.isEmpty()) {
+                st.push(s.charAt(i));
+            } else if((st.peek() == '(' && s.charAt(i) == ')') || (st.peek() == '[' && s.charAt(i) == ']')
+                     || (st.peek() == '{' && s.charAt(i) == '}')) {
+                st.pop();
+            } else {
+                st.push(s.charAt(i));
+            }
+        }
+        if(st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
