b1410e8aeb347b8d98d69704f276564c44ab752c
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-10 12:47:48 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1056 ms, Memory: 58640 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numCount = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        int[][] numArr = new int[numCount][2];
+
+        for(int i = 0; i <numArr.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            numArr[i][0] = Integer.parseInt(st.nextToken());
+            numArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numArr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1];
+            }
+        });
+
+        for(int i = 0; i < numArr.length; i++) {
+            System.out.println(numArr[i][0] + " " + numArr[i][1]);
+        }
+    }
+}
