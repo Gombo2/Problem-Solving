0d38fdd8ee97489162918ab7ebd0fa16aa38a3b4
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-14 21:04:49 +0900

    [Silver V] Title: 셀프 넘버, Time: 100 ms, Memory: 13948 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -5,30 +5,28 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        Set<Integer> numHashSet = new HashSet<>();
-        int num = 1;
-        int curSum = 0;
-        for (int i = 1; i <= 10000; i++) {
-            numHashSet.add(i);
-        }
+        boolean[] d = new boolean[10001];
 
-        for(int j = 1; j<= 10000; j++) {
-            num = j;
-            curSum = 0;
-            while(num != 0) {
-                curSum += num % 10;
-                num /= 10;
+        for (int i = 1; i < 10001; i++) {
+            int n = i;
+            int j = i;
+
+            while (j > 0) {
+                n += j % 10;
+                j /= 10;
             }
-            curSum += j;
-            numHashSet.remove(curSum);
-        }
 
-        Iterator<Integer> it = numHashSet.iterator();
-        while(it.hasNext()) {
-            sb.append(it.next()).append("\n");
+            if (n < 10001) {
+                d[n] = true;
+            }
         }
+        for (int i = 1; i < 10001; i++) {
+            if (!d[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        
         System.out.println(sb);
     }
 }
