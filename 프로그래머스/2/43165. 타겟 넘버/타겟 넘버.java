b003f361bff20cbc0849954f5d7fb456cb4d24da
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-18 17:02:27 +0900

    [level 2] Title: 타겟 넘버, Time: 5.34 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -6,15 +6,15 @@ class Solution {
     }
     
     private static void dfs(int depth, int[] numbers, int target, int sum) {
-        if(depth == numbers.length) {
-            if(sum == target) {
+        if(depth == numbers.length) { //끝까지 탐색완료했을때
+            if(sum == target) { // 연산결과가 target과 같은경우
                 answer++;
             }
             return;
         }
         
-        dfs(depth + 1, numbers, target, sum + numbers[depth]);
+        dfs(depth + 1, numbers, target, sum + numbers[depth]); // +
         
-        dfs(depth + 1, numbers, target, sum - numbers[depth]);
+        dfs(depth + 1, numbers, target, sum - numbers[depth]); // -
     }
 }
