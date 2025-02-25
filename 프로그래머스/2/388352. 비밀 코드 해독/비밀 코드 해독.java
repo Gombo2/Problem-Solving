52bade87111d0239c7b3bfdd5f447eb248c2facf
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-02-25 17:36:22 +0900

    [level 2] Title: 비밀 코드 해독, Time: 64.11 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -3,24 +3,28 @@ import java.util.*;
 public class Solution {
     public int solution(int n, int[][] q, int[] ans) {
         List<List<Integer>> combinations = new ArrayList<>();
+        //각 케이스 List로 만들고 List에 담음.
         allCombinations(n, 5, 1, new ArrayList<>(), combinations);
+        //모든 경우의수
         int count = 0;
+        
         for (List<Integer> combination : combinations) {
             if (isTrue(combination, q, ans)) {
                 count++;
             }
         }
+        
         return count;
     }
 
-    private void allCombinations(int n, int r, int start, List<Integer> current, List<List<Integer>> result) {
-        if (current.size() == r) {
+    private void allCombinations(int n, int depth, int start, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == depth) {
             result.add(new ArrayList<>(current));
             return;
         }
         for (int i = start; i <= n; i++) {
             current.add(i);
-            allCombinations(n, r, i + 1, current, result);
+            allCombinations(n, depth, i + 1, current, result);
             current.remove(current.size() - 1);
         }
     }
