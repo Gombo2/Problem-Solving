fef1d2c258143a3d385fa0e0950d92a466f98e6e
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-09 19:47:38 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5600 ms, Memory: 368784 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+
+        System.out.println(a.multiply(b));
+
+    }
+}
